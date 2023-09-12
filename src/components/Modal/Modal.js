2e61e1f054d1c4ab7540e2e884935886b63b761a
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2023-09-12 22:03:26 +0300

    add hooks

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,43 +1,50 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import { ModalWrapper, ModalContent } from './Modal.styles';
 
-class MyModal extends Component {
-  render() {
-    const { isOpen, onClose, imageURL } = this.props;
+function MyModal({ isOpen, onClose, imageURL }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    return (
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onClose}
-        contentLabel="Modal"
-        ariaHideApp={false}
-        style={{
-          overlay: {
-            zIndex: 9999,
-            backgroundColor: 'rgba(0, 0, 0, 0.8)',
-          },
-          content: {
-            position: 'relative',
-            top: 'auto',
-            left: 'auto',
-            right: 'auto',
-            bottom: 'auto',
-            border: 'none',
-            background: 'none',
-            padding: 0,
-          },
-        }}
-      >
-        <ModalWrapper>
-          <ModalContent>
-            <img src={imageURL} alt=""/>
-            <button onClick={onClose}>Закрыть</button>
-          </ModalContent>
-        </ModalWrapper>
-      </Modal>
-    );
-  }
+  useEffect(() => {
+    setModalIsOpen(isOpen);
+  }, [isOpen]);
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    onClose();
+  };
+
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Modal"
+      ariaHideApp={false}
+      style={{
+        overlay: {
+          zIndex: 9999,
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        },
+        content: {
+          position: 'relative',
+          top: 'auto',
+          left: 'auto',
+          right: 'auto',
+          bottom: 'auto',
+          border: 'none',
+          background: 'none',
+          padding: 0,
+        },
+      }}
+    >
+      <ModalWrapper>
+        <ModalContent>
+          <img src={imageURL} alt=""/>
+          <button onClick={closeModal}>Закрыть</button>
+        </ModalContent>
+      </ModalWrapper>
+    </Modal>
+  );
 }
 
 export default MyModal;
